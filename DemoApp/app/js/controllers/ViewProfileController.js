ddf52128f68c9b7c02f91a48e8548f8e680b36ea
Joe Eames <joeeames@gmail.com>
2014-08-18 12:09:21 -0600

    removed extraneous directory

--- DemoApp/app/js/controllers/ViewProfileController.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-eventsApp.controller('ViewProfileController',
-    function ViewProfileController($scope, $routeParams, userData) {
-        $scope.user = userData.getUser($routeParams.userName);
-    }
-);
--- DemoApp/server.bat
+++
@@ -1,2 +0,0 @@
-cd app
-node ..\scripts\web-server.js
